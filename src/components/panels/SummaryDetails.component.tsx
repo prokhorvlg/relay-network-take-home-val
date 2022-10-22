c2a06fdbf5c82801d5769112a1ecfe553b6a7ce4
Valentin Sigalov <vlgstudent036@gmail.com>
2022-10-22 19:29:27 -0400

    Added unit tests for summary details component

--- src/components/panels/SummaryDetails.component.tsx
+++ src/components/panels/SummaryDetails.component.tsx
@@ -10,7 +10,7 @@ interface SummaryDetailsProps {
 const SummaryDetails = ({ text, segment, selected }: SummaryDetailsProps) => {
     if (!segment.percentage) return null
     return (
-        <div className={"details " + (selected ? "selected" : "")}>
+        <div data-testid="summary-details" className={"details " + (selected ? "selected" : "")}>
             <p>{text}</p>
             <h2>{segment.name} - {formatNumberWithCommas(segment.count || 0)}</h2>
             <p className="percent">{segment.percentage.toFixed(2)}%</p>
